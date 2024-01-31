b3d11bf2af59648a6438159f0de5b2bdd24eab72
HYE SEONG LEE <hyeseong @dtto9.com>
2024-01-31 16:26:46 +0900

    feat. ADD - Cart 엔티티에 ID 자동 생성 전략 추가
    - Cart 엔티티의 id 필드에 @GeneratedValue 어노테이션을 추가하여, ID 값이 자동으로 생성되도록 설정.
    - GenerationType.AUTO를 사용하여 데이터베이스 구현체가 최적의 ID 생성 전략을 자동으로 선택하도록 함.
    - Member 엔티티와의 관계를 OneToOne으로 매핑하고, member_id를 외래키로 사용하여 조인.

---
+++ src/main/java/com/shop/shopmasterclone/entity/Cart.java
@@ -0,0 +1,21 @@
+package com.shop.shopmasterclone.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name="cart")
+@Getter
+@Setter
+public class Cart {
+    @Id
+    @Column(name="cart_id")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @OneToOne
+    @JoinColumn(name="member_id")
+    private Member member;
+
+}
