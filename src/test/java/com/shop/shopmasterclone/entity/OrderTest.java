0ab812632dc84e50a6377faea25d6a20bf6de533
HYE SEONG LEE <hyeseong @dtto9.com>
2024-02-01 10:08:18 +0900

    feat: 주문 항목의 지연 로딩 테스트 추가 및 OrderItem 엔티티 수정
    Order 엔티티와 OrderItem 엔티티 간의 관계에 지연 로딩(Lazy Loading)을 적용하고, 이를 검증하는 테스트를 추가했습니다. OrderItem 엔티티에서 Order로의 매핑에 FetchType.LAZY를 설정하여, OrderItem 조회 시 연관된 Order가 즉시 로드되지 않도록 구성했습니다.
    
    주요 변경 사항은 다음과 같습니다:
    1. OrderItem 엔티티 내의 Order 필드에 FetchType.LAZY 옵션을 추가하여 지연 로딩을 활성화.
    2. OrderTest 클래스에 `givenOrderItemId_whenFindById_thenOrderShouldBeLazyLoaded` 테스트 메서드를 추가. 이 테스트는 OrderItem 조회 시 연관된 Order가 초기화되지 않은 프록시 객체로 남아 있음을 확인하고, 실제 데이터에 접근했을 때만 초기화되는 것을 검증.
    
    이 변경은 데이터베이스의 성능 최적화와 필요에 따른 데이터 로딩을 통해 애플리케이션의 효율성을 높이는 데 기여합니다.

--- src/test/java/com/shop/shopmasterclone/entity/OrderTest.java
+++ src/test/java/com/shop/shopmasterclone/entity/OrderTest.java
@@ -3,10 +3,12 @@ package com.shop.shopmasterclone.entity;
 import com.shop.shopmasterclone.constant.ItemSellStatus;
 import com.shop.shopmasterclone.repository.ItemRepository;
 import com.shop.shopmasterclone.repository.MemberRepository;
+import com.shop.shopmasterclone.repository.OrderItemRepository;
 import com.shop.shopmasterclone.repository.OrderRepository;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityNotFoundException;
 import jakarta.persistence.PersistenceContext;
+import org.hibernate.Hibernate;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -32,8 +34,40 @@ class OrderTest {
     @Autowired
     MemberRepository memberRepository;
 
+    @Autowired
+    OrderItemRepository orderItemRepository;
+
     @PersistenceContext
     EntityManager entityManager;
+
+    @Test
+    @DisplayName("주문 항목 조회 시 주문은 지연 로딩되어야 함")
+    public void givenOrderItemId_whenFindById_thenOrderShouldBeLazyLoaded() {
+        // Given
+        Order order = this.createOrder();   //새로운 Order와 OrderItem을 생성하고 저장
+        Long orderItemId = order.getOrderItems().get(0).getId();
+
+        // 영속성 컨텍스트를 초기화합니다.
+        // 이는 OrderItem 조회 시 새로운 영속성 컨텍스트 세션에서 수행되도록 함으로써 테스트의 독립성을 보장함
+        entityManager.flush();
+        entityManager.clear();
+
+
+        // When
+        //  지연 로딩이 작동하고 있음을 확인합니다. 이 시점에서 Order는 초기화되지 않아야 합니다.
+        OrderItem orderItem = orderItemRepository.findById(orderItemId)
+                .orElseThrow(EntityNotFoundException::new);
+
+        // Then
+        assertFalse(Hibernate.isInitialized(orderItem.getOrder()));
+
+        // 실제 Order 데이터에 접근
+        orderItem.getOrder().getOrderDate();
+
+        // 이제 Order가 초기화되었는지 확인
+        // Order가 이제 초기화되었음을 확인
+        assertTrue(Hibernate.isInitialized(orderItem.getOrder()));
+    }
 
     public Order createOrder(){
         Order order = new Order();
