fe653d99cafc6f9afaa015b419e522733b2cb983
HYE SEONG LEE <hyeseong @dtto9.com>
2024-02-01 09:44:00 +0900

    feat: 추가 Order 엔티티에 orderItems 필드
    Order 엔티티에 orderItems 필드를 추가하여, Order와 OrderItem 간의 일대다 관계를 정의했습니다. 이 변경은 Order 엔티티가 관련 OrderItem 객체들의 목록을 관리할 수 있게 하며, 각 주문에 대한 주문 항목들을 직접 추적할 수 있게 합니다. 추가된 필드는 CascadeType.ALL과 orphanRemoval=true 옵션을 사용하여, OrderItem의 생명주기가 Order 엔티티에 종속되도록 설정되었습니다. 이로써, Order 엔티티의 상태 변경이 연관된 OrderItem 객체들에도 적용됩니다.

--- src/main/java/com/shop/shopmasterclone/entity/Order.java
+++ src/main/java/com/shop/shopmasterclone/entity/Order.java
@@ -2,11 +2,17 @@ package com.shop.shopmasterclone.entity;
 
 import com.shop.shopmasterclone.constant.OrderStatus;
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name="orders")
+@Getter
+@Setter
 public class Order {
 
     @Id
@@ -21,6 +27,9 @@ public class Order {
 
     @Enumerated(EnumType.STRING)
     private OrderStatus orderStatus;
+
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<OrderItem> orderItems = new ArrayList<>();
 
     private LocalDateTime regTime;
     private LocalDateTime updateTime;
