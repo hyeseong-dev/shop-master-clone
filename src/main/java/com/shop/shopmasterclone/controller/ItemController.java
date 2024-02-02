e516c77e11007f67127a9c6ff023ffe1b7178314
HYE SEONG LEE <hyeseong @dtto9.com>
2024-02-02 15:37:32 +0900

    feat. ADD - 관리자의 상품 목록을 조회를 위한 '/admin/items' GET 엔드포인트 추가

--- src/main/java/com/shop/shopmasterclone/controller/ItemController.java
+++ src/main/java/com/shop/shopmasterclone/controller/ItemController.java
@@ -1,14 +1,19 @@
 package com.shop.shopmasterclone.controller;
 
 import com.shop.shopmasterclone.dto.ItemFormDto;
-import com.shop.shopmasterclone.dto.MemberFormDto;
-import com.shop.shopmasterclone.entity.Member;
+import com.shop.shopmasterclone.dto.ItemSearchDto;
+
+import com.shop.shopmasterclone.entity.Item;
+
 import com.shop.shopmasterclone.service.ItemService;
-import com.shop.shopmasterclone.service.MemberService;
+
 import jakarta.persistence.EntityNotFoundException;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -16,6 +21,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
+import java.util.Optional;
 
 /**
  * 상품 관련 기능을 처리하는 컨트롤러 클래스입니다. 상품 등록, 수정 관련 HTTP 요청을 처리하며,
@@ -26,6 +32,40 @@ import java.util.List;
 public class ItemController {
 
     private final ItemService itemService;
+
+    /**
+     * 상품 관리 페이지를 조회합니다. 이 메소드는 관리자가 상품 목록을 조회할 때 사용됩니다.
+     * 선택적으로 페이지 번호를 매개변수로 받아 해당 페이지의 상품 목록과 페이징 정보를 반환합니다.
+     *
+     * @param itemSearchDto 상품 검색 조건을 담은 데이터 전송 객체
+     * @param page 조회할 페이지 번호 (Optional)
+     * @param model 뷰에 전달할 모델 객체
+     * @return 상품 관리 페이지의 뷰 이름
+     */
+    @GetMapping(value="/admin/items")
+    public String itemMange(
+            ItemSearchDto itemSearchDto,
+            @RequestParam("page") Optional<Integer> page,
+            Model model
+    ){
+        Pageable pageable = PageRequest.of(page.orElse(0), 3);
+        Page<Item> items = itemService.getAdminItemPage(itemSearchDto, pageable);
+        model.addAttribute("items", items);
+        model.addAttribute("itemSearchDto", itemSearchDto);
+        model.addAttribute("maxPage", 5);
+        return "item/itemMng";
+
+    }
+
+    /**
+     * 특정 상품의 상세 정보 페이지를 조회합니다. 상품 ID를 통해 상품 정보를 조회하고,
+     * 해당 정보를 모델에 바인딩하여 뷰에 전달합니다. 상품이 존재하지 않을 경우, 에러 메시지를 포함해 반환합니다.
+     *
+     * @param itemId 조회할 상품의 ID
+     * @param model 뷰에 전달할 데이터를 담은 모델 객체
+     * @return 상품 상세 정보 페이지의 뷰 이름
+     * @throws EntityNotFoundException 상품 ID에 해당하는 상품이 없을 경우 발생
+     */
 
     @GetMapping(value = "/admin/item/{itemId}")
     public String itemDtl(@PathVariable("itemId") Long itemId, Model model){
@@ -50,13 +90,16 @@ public class ItemController {
     }
 
     /**
-     * 상품 정보를 수정하는 메서드입니다. HTTP POST 요청을 처리하며, 입력된 상품 정보를 업데이트합니다.
+     * 특정 상품의 정보를 수정합니다. 상품 ID를 통해 기존 상품 정보를 조회하고, 입력 받은 정보로 업데이트합니다.
+     * 상품 이미지도 함께 업데이트되며, 새로운 이미지가 제공될 경우 기존 이미지를 대체합니다.
      *
-     * @param itemFormDto        수정할 상품 정보를 담은 DTO 객체
-     * @param bindingResult      입력 데이터의 유효성 검사 결과
-     * @param itemImgFileList    상품 이미지 파일 리스트
-     * @param model              뷰 템플릿에 전달할 데이터 모델
-     * @return                   수정 완료 후 리다이렉트 페이지 경로
+     * @param itemFormDto 수정할 상품 정보가 담긴 DTO 객체
+     * @param bindingResult 입력 데이터의 유효성 검사 결과
+     * @param itemImgFileList 수정할 상품 이미지 파일 리스트
+     * @param model 뷰에 전달할 데이터를 담은 모델 객체
+     * @return 상품 수정 후 리다이렉션할 경로
+     * @throws EntityNotFoundException 상품 ID에 해당하는 상품이 데이터베이스에 없을 경우
+     * @throws IllegalArgumentException 이미지 파일 리스트가 비어 있거나 다른 예외가 발생한 경우
      */
     @PostMapping(value = "/admin/item/{itemId}")
     public String itemUpdate(@Valid ItemFormDto itemFormDto, BindingResult bindingResult,
@@ -81,11 +124,13 @@ public class ItemController {
     }
 
     /**
-     * 새로운 상품을 등록하는 페이지로 이동하는 메서드입니다.
+     * 새로운 상품을 등록하기 위한 페이지로 이동합니다. 이 메소드는 상품 등록 폼을 제공합니다.
+     * 상품 등록 폼 DTO를 모델에 포함시켜 뷰에 전달합니다.
      *
-     * @param model  뷰 템플릿에 전달할 데이터 모델
-     * @return       상품 등록 페이지의 경로
+     * @param model 뷰에 전달할 데이터를 담은 모델 객체
+     * @return 상품 등록 페이지의 뷰 이름
      */
+
     @GetMapping(value="/admin/item/new")
     public String itemForm(Model model){
         model.addAttribute("itemFormDto", new ItemFormDto());
@@ -93,13 +138,16 @@ public class ItemController {
     }
 
     /**
-     * 새로운 상품을 등록하는 메서드입니다. HTTP POST 요청을 처리하며, 입력된 상품 정보를 등록합니다.
+     * 사용자로부터 입력 받은 상품 정보와 이미지 파일들을 기반으로 새로운 상품을 등록합니다.
+     * 입력 데이터의 유효성 검사를 수행하고, 상품 정보와 이미지 파일이 유효할 경우 상품을 등록합니다.
+     * 상품 이미지 파일은 첫 번째 이미지를 대표 이미지로 설정합니다.
      *
-     * @param itemFormDto        등록할 상품 정보를 담은 DTO 객체
-     * @param bindingResult      입력 데이터의 유효성 검사 결과
-     * @param itemImgFileList    상품 이미지 파일 리스트
-     * @param model              뷰 템플릿에 전달할 데이터 모델
-     * @return                   등록 완료 후 리다이렉트 페이지 경로
+     * @param itemFormDto 등록할 상품 정보가 담긴 DTO 객체
+     * @param bindingResult 입력 데이터의 유효성 검사 결과
+     * @param itemImgFileList 등록할 상품 이미지 파일 리스트
+     * @param model 뷰에 전달할 데이터를 담은 모델 객체
+     * @return 상품 등록 후 리다이렉션할 경로
+     * @throws IllegalArgumentException 첫 번째 이미지 파일이 비어있거나 예외가 발생한 경우
      */
     @PostMapping(value="/admin/item/new")
     public String itemNew(
