547c2502b1aa5cabc981feaa8a4b580eaf38dee8
HYE SEONG LEE <hyeseong @dtto9.com>
2024-02-02 15:05:38 +0900

    feat. ADD - queryDsl사용을 위한 ItemRepositoryCustom 명세

--- src/main/java/com/shop/shopmasterclone/repository/ItemRepositoryCustom.java
+++ src/main/java/com/shop/shopmasterclone/repository/ItemRepositoryCustom.java
@@ -1,6 +1,12 @@
 package com.shop.shopmasterclone.repository;
 
 
+import com.shop.shopmasterclone.dto.ItemSearchDto;
+import com.shop.shopmasterclone.entity.Item;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
 public interface ItemRepositoryCustom {
 
+    Page<Item> getAdminItemPage(ItemSearchDto itemSearchDto, Pageable pageable);
 }
