bcae36764e949a3aa62ba1160441c339e8a7a950
HYE SEONG LEE <hyeseong @dtto9.com>
2024-02-02 15:38:07 +0900

    test. ADD - 관리자의 상품 목록을 조회를 위한 '/admin/items' GET 엔드포인트 추가에 따른 '관리자 상품 관리 페이지 페이징 조회 테스트' 테스트 코드 추가

--- src/test/java/com/shop/shopmasterclone/controller/ItemControllerTest.java
+++ src/test/java/com/shop/shopmasterclone/controller/ItemControllerTest.java
@@ -1,5 +1,6 @@
 package com.shop.shopmasterclone.controller;
 
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,6 +22,19 @@ class ItemControllerTest {
 
     @Autowired
     MockMvc mockMvc;
+
+    @Test
+    @DisplayName("관리자 상품 관리 페이지 페이징 조회 테스트")
+    @WithMockUser(username="admin", roles="ADMIN")
+    public void givenAdminAccessAndPageNumber_whenViewingItemManagePage_thenReturnsCorrectPage() throws Exception {
+        int testPage = 1; // 테스트할 페이지 번호
+        mockMvc.perform(MockMvcRequestBuilders.get("/admin/items")
+                        .param("page", String.valueOf(testPage)))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.model().attributeExists("items"))
+                .andExpect(MockMvcResultMatchers.view().name("item/itemMng"))
+                .andDo(MockMvcResultHandlers.print());
+    }
 
     @Test
     @DisplayName("상품 등록 페이지 권한 테스트")
