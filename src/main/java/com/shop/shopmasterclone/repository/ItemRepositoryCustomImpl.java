ff3c3e3d482530e561e3fa40d294dc4a99c8c5c8
HYE SEONG LEE <hyeseong @dtto9.com>
2024-02-02 15:05:57 +0900

    feat. ADD - ItemRepositoryCustom 명세에 따른 ItemRepositoryCustomImpl 구현

---
+++ src/main/java/com/shop/shopmasterclone/repository/ItemRepositoryCustomImpl.java
@@ -0,0 +1,73 @@
+package com.shop.shopmasterclone.repository;
+
+import com.querydsl.core.QueryResults;
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import com.shop.shopmasterclone.constant.ItemSellStatus;
+import com.shop.shopmasterclone.dto.ItemSearchDto;
+import com.shop.shopmasterclone.entity.Item;
+import com.shop.shopmasterclone.entity.QItem;
+import jakarta.persistence.EntityManager;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+import org.thymeleaf.util.StringUtils;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public class ItemRepositoryCustomImpl implements ItemRepositoryCustom{
+
+    private JPAQueryFactory queryFactory;
+
+    public ItemRepositoryCustomImpl(EntityManager entityManager){
+        this.queryFactory=new JPAQueryFactory(entityManager);
+    }
+
+    private BooleanExpression searchSellStatusEq(ItemSellStatus searchSellStatus){
+        return searchSellStatus == null ? null : QItem.item.itemSellStatus.eq(searchSellStatus);
+    }
+
+    private BooleanExpression regDtsAfter(String searchDateType){
+
+        if (StringUtils.equals("all", searchDateType) || searchDateType == null) {
+            return null;
+        }
+
+        LocalDateTime dateTime = LocalDateTime.now();
+
+        dateTime = switch (searchDateType) {
+            case "1d" -> dateTime.minusDays(1);
+            case "1w" -> dateTime.minusWeeks(1);
+            case "1m" -> dateTime.minusMonths(1);
+            case "6m" -> dateTime.minusMonths(6);
+            default -> throw new IllegalArgumentException("Invalid searchDateType: " + searchDateType);
+        };
+
+        return QItem.item.regTime.after(dateTime);
+    }
+
+    private BooleanExpression searchByLike(String searchBy, String searchQuery){
+        if ("itemNm".equals(searchBy)) {
+            return QItem.item.itemNm.like("%" + searchQuery + "%");
+        } else if ("createdBy".equals(searchBy)) {
+            return QItem.item.createdBy.like("%" + searchQuery + "%");
+        }
+        return null;
+    }
+
+    @Override
+    public Page<Item> getAdminItemPage(ItemSearchDto itemSearchDto, Pageable pageable) {
+        List<Item> content = queryFactory
+                .selectFrom(QItem.item)
+                .where(regDtsAfter(itemSearchDto.getSearchDateType()),
+                        searchSellStatusEq(itemSearchDto.getSearchSellStatus()),
+                        searchByLike(itemSearchDto.getSearchBy(), itemSearchDto.getSearchQuery()))
+                .orderBy(QItem.item.id.desc())
+                .offset(pageable.getOffset())
+                .limit(pageable.getPageSize())
+                .fetch();
+
+        return new PageImpl<>(content, pageable, content.size());
+    }
+}
