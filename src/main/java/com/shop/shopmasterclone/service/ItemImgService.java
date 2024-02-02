8321487ba84a0932d7a9342bb37b4898c2476a05
HYE SEONG LEE <hyeseong @dtto9.com>
2024-02-02 11:02:30 +0900

    feat. ADD - ItemService 클래스에 주석 추가 및 updateItemImg 메소드 추가

--- src/main/java/com/shop/shopmasterclone/service/ItemImgService.java
+++ src/main/java/com/shop/shopmasterclone/service/ItemImgService.java
@@ -2,18 +2,19 @@ package com.shop.shopmasterclone.service;
 
 import com.shop.shopmasterclone.entity.ItemImg;
 import com.shop.shopmasterclone.repository.ItemImgRepository;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.java.Log;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 import org.thymeleaf.util.StringUtils;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.util.UUID;
 
+/**
+ * 상품 이미지 관련 서비스를 제공하는 클래스입니다. 상품 이미지의 업데이트와 저장을 담당합니다.
+ */
 @Service
 @RequiredArgsConstructor
 @Transactional
@@ -24,6 +25,35 @@ public class ItemImgService {
     private final ItemImgRepository itemImgRepository;
     private final FileService fileService;
 
+    /**
+     * 주어진 상품 이미지 ID를 사용하여 이미지를 업데이트합니다.
+     *
+     * @param itemImgId     업데이트할 상품 이미지의 ID
+     * @param itemImgFile   업데이트할 상품 이미지 파일
+     * @throws Exception    이미지 업데이트 또는 파일 처리 중 발생할 수 있는 예외를 처리합니다.
+     */
+    public void updateItemImg(Long itemImgId, MultipartFile itemImgFile) throws Exception{
+        if(!itemImgFile.isEmpty()){
+            ItemImg savedItemImg = itemImgRepository.findById(itemImgId).orElseThrow(EntityNotFoundException::new);
+
+            // 기존 이미지 파일 삭제
+            if(!StringUtils.isEmpty(savedItemImg.getImgName()))
+                fileService.deleteFile(itemImgLocation + "/" + savedItemImg.getImgName());
+
+            String oriImgName = itemImgFile.getOriginalFilename();
+            String imgName = fileService.uploadFile(itemImgLocation, oriImgName, itemImgFile.getBytes());
+            String imgUrl = "/images/item/" + imgName;
+            savedItemImg.updateItemImg(oriImgName, imgName, imgUrl);
+        }
+    }
+
+    /**
+     * 새로운 상품 이미지를 저장합니다.
+     *
+     * @param itemImg       저장할 상품 이미지 엔티티
+     * @param itemImgFile   저장할 상품 이미지 파일
+     * @throws Exception    이미지 업로드 및 저장 중 발생할 수 있는 예외를 처리합니다.
+     */
     public void saveItemImg(ItemImg itemImg, MultipartFile itemImgFile) throws Exception{
         String oriImgName = itemImgFile.getOriginalFilename();
         String imgName = "";
@@ -39,4 +69,6 @@ public class ItemImgService {
         itemImg.updateItemImg(oriImgName, imgName, imgUrl);
         itemImgRepository.save(itemImg);
     }
+
+
 }
