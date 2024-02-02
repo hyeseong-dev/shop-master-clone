0c851435898f917a13e381d2744d340266d518d0
HYE SEONG LEE <hyeseong @dtto9.com>
2024-02-02 15:06:27 +0900

    test. ADD - 구현한 ItemRepositoryCustomImpl 테스트를 위한 테스트 코드 작성

--- src/test/java/com/shop/shopmasterclone/repository/ItemRepositoryTest.java
+++ src/test/java/com/shop/shopmasterclone/repository/ItemRepositoryTest.java
@@ -4,6 +4,7 @@ import com.querydsl.core.BooleanBuilder;
 import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.shop.shopmasterclone.constant.ItemSellStatus;
+import com.shop.shopmasterclone.dto.ItemSearchDto;
 import com.shop.shopmasterclone.entity.Item;
 import com.shop.shopmasterclone.entity.QItem;
 import jakarta.persistence.EntityManager;
@@ -21,6 +22,7 @@ import org.thymeleaf.util.StringUtils;
 import java.time.LocalDateTime;
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 
@@ -33,6 +35,31 @@ class ItemRepositoryTest {
 
     @PersistenceContext
     EntityManager em;
+
+    @Test
+    @DisplayName("Given 상품 검색 조건 When 관리자 페이지에서 상품을 조회 Then 조건에 맞는 상품만 표시되어야 함")
+    public void given_SearchConditions_when_AdminSearchesItems_then_OnlyMatchingItemsShouldBeDisplayed() {
+        // Given: 상품 검색 조건을 설정
+        Item item = new Item();
+        item.setItemNm("Test Item");
+        item.setPrice(10000);
+        item.setItemDetail("Test Item Detail");
+        item.setItemSellStatus(ItemSellStatus.SELL);
+        itemRepository.save(item);
+
+        ItemSearchDto itemSearchDto = new ItemSearchDto();
+        itemSearchDto.setSearchDateType("all"); // 모든 날짜
+        itemSearchDto.setSearchSellStatus(ItemSellStatus.SELL); // 판매 중인 상품
+        itemSearchDto.setSearchBy("itemNm"); // 상품명으로 검색
+        itemSearchDto.setSearchQuery("Test"); // 'Test'를 포함하는 상품명
+
+        // When: 관리자 페이지에서 상품을 조회
+        Page<Item> resultPage = itemRepository.getAdminItemPage(itemSearchDto, PageRequest.of(0, 10));
+
+        // Then: 조건에 맞는 상품만 표시되어야 함
+        assertThat(resultPage.getContent()).hasSize(1);
+        assertThat(resultPage.getContent().get(0).getItemNm()).isEqualTo("Test Item");
+    }
 
     @Test
     @DisplayName("상품 저장 테스트")
