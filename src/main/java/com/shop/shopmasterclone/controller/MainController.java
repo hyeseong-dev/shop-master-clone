4454aa617556b0c09cf63ba3c4dff3e84f3e7ae0
HYE SEONG LEE <hyeseong @dtto9.com>
2024-02-05 10:48:17 +0900

    feat. UPDATE - 메인 페이지 컨트롤러 기능 확장
    - 메인 페이지에 표시될 상품 목록 조회 기능을 추가하였습니다. 사용자는 상품 검색 조건(상품명, 카테고리, 판매 상태 등)을 기반으로 원하는 상품 목록을 필터링할 수 있습니다.
    - 페이지네이션 처리를 위해 Pageable 인터페이스를 활용하였으며, 기본적으로 한 페이지 당 6개의 상품을 표시하도록 구성하였습니다. 사용자는 페이지 번호를 통해 추가 상품 목록을 조회할 수 있습니다.
    - Model 객체에 상품 목록, 상품 검색 조건, 최대 페이지 수 정보를 추가하여, 뷰에서 이를 활용할 수 있도록 하였습니다.
    - 이 변경을 통해 메인 페이지의 사용자 경험을 개선하고, 상품 검색 및 조회 기능의 접근성을 높였습니다.
    
    변경된 메서드: main
    추가된 매개변수: ItemSearchDto itemSearchDto, Optional<Integer> page, Model model

--- src/main/java/com/shop/shopmasterclone/controller/MainController.java
+++ src/main/java/com/shop/shopmasterclone/controller/MainController.java
@@ -1,13 +1,48 @@
 package com.shop.shopmasterclone.controller;
 
+import com.shop.shopmasterclone.dto.ItemSearchDto;
+import com.shop.shopmasterclone.dto.MainItemDto;
+import com.shop.shopmasterclone.service.ItemService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.Optional;
+
 @Controller
+@RequiredArgsConstructor
 public class MainController {
 
+    private final ItemService itemService;
+
+    /**
+     * 메인 페이지를 위한 컨트롤러 메서드입니다. 이 메서드는 메인 페이지에 표시될 상품 목록을 조회하여 반환합니다.
+     * 사용자는 상품 검색 조건을 기반으로 상품 목록을 필터링할 수 있습니다. 검색 조건에는 상품명, 카테고리,
+     * 판매 상태 등이 포함될 수 있습니다.
+     *
+     * <p>페이징 처리가 적용되어 있으며, 한 페이지 당 상품 목록의 수는 6개로 제한됩니다. 사용자는 페이지 번호를
+     * 통해 추가 상품 목록을 조회할 수 있습니다.</p>
+     *
+     * @param itemSearchDto 상품 검색 조건을 담은 DTO. 검색 조건이 없는 경우 모든 상품이 조회됩니다.
+     * @param page 요청된 페이지 번호. 페이지 번호는 0부터 시작합니다. Optional 객체를 사용하여, 페이지 번호가 제공되지 않은 경우 기본값으로 0을 사용합니다.
+     * @param model 뷰에 데이터를 전달하기 위한 Model 객체. 조회된 상품 목록, 상품 검색 조건, 최대 페이지 수 등의 정보를 포함합니다.
+     * @return 메인 페이지의 뷰 이름을 나타내는 문자열. 조회된 상품 목록과 함께 "main" 뷰로 사용자를 리다이렉트합니다.
+     */
     @GetMapping(value="/")
-    public String main(){
+    public String main(
+            ItemSearchDto itemSearchDto,
+            Optional<Integer> page,
+            Model model
+    ){
+        Pageable pageable = PageRequest.of(page.isPresent() ? page.get() : 0, 6);
+        Page<MainItemDto> items = itemService.getMainItemPage(itemSearchDto, pageable);
+        model.addAttribute("items", items);
+        model.addAttribute("itemsSearchDto", itemSearchDto);
+        model.addAttribute("maxPage", 5);
         return "main";
     }
 
