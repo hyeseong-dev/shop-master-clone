1893124a7cb0fcd03aee24e3feb0ca7f71354f08
HYE SEONG LEE <hyeseong @dtto9.com>
2024-02-06 16:11:46 +0900

    feat. update - CustomAuthenticationEntryPoint클래스 commence 메서드 로깅 기능 추가

--- src/main/java/com/shop/shopmasterclone/config/CustomAuthenticationEntryPoint.java
+++ src/main/java/com/shop/shopmasterclone/config/CustomAuthenticationEntryPoint.java
@@ -3,7 +3,8 @@ package com.shop.shopmasterclone.config;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import org.springframework.security.core.Authentication;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
@@ -12,8 +13,11 @@ import java.io.IOException;
 
 @Component
 public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint {
+    private static final Logger logger = LoggerFactory.getLogger(CustomAuthenticationEntryPoint.class);
+
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
+        logger.error("Unauthorized error: {}", authException.getMessage());
         response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized");
     }
 }
